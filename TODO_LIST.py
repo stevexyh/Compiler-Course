2d10ca1e9d9c54edd1520b681a23df234292427a
stevexyh@hotmail.com <stevexyh@hotmail.com>
2020-06-24 18:08:55 +0800

    docs(README): Updated TODO list
    - Added TODO list in README.md
    - Edited TODO_LIST.py

--- TODO_LIST.py
+++ TODO_LIST.py
@@ -18,7 +18,11 @@
 '''
 
 
-# TODO(Steve X): 输出程序中各种符号定义(符号表形式)
+# DONE(Steve X): 使用 `flex` 实现词法分析器
+# DONE(Steve X): 识别 `MiniPascal` 语法要求
+# DONE(Steve X): 输入 `MiniPascal` 源文件
+# DONE(Steve X): 识别文法中的分隔符、算符等字符关键字(应该是 `yacc` 部分)
+# DONE(Steve X): 输出程序中各种符号定义(符号表形式)
 # TODO(Steve X): 输出程序的四元式
 # TODO(Steve X): 输出抽象语法树, 通过 Graphviz 显示
 # TODO(Steve X): 简单的错误诊断(所在行, 变量不存在, 重复定义等)
